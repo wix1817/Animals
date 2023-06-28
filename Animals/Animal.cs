c6a5f901be33a146bb500790ceabe4de4baa76b4
Aliaksei Pustavy <wix1817@gmail.com>
2023-06-28 18:20:29 +0200

    Update Animals.sln, Animal.cs, and 2 more files...

---
+++ Animals/Animal.cs
@@ -0,0 +1,26 @@
+﻿namespace Animals;
+
+public abstract class Animal
+{
+    string Name;
+    
+    public void SetName(string name)
+    {
+        Name = name;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+
+    public abstract void Eat();
+}
+
+public class Dog : Animal
+{
+    public override void Eat()
+    {
+        Console.WriteLine("Dog Eat");
+    }
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using Animals;
+
+Console.Write("Input dog name: "); 
+var name = Console.ReadLine();
+var dog = new Dog();
+dog.SetName(name);
+Console.WriteLine(dog.GetName());
+dog.Eat();
